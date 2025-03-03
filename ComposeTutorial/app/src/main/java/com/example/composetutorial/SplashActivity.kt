86953db74b72dad0d34976c0dd2a0bf17de939ca
Oliver Saari <oliver.saari@student.oulu.fi>
2025-03-03 17:15:40 +0200

    Bounce animation to splashscreen

--- ComposeTutorial/app/src/main/java/com/example/composetutorial/SplashActivity.kt
+++ ComposeTutorial/app/src/main/java/com/example/composetutorial/SplashActivity.kt
@@ -6,13 +6,17 @@ import android.os.Handler
 import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
@@ -26,16 +30,25 @@ class SplashActivity : ComponentActivity() {
             }
         }
 
-        // Delay transition to MainActivity
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 3000) // 3-second delay
+        }, 3000)
     }
 }
 
 @Composable
 fun SplashScreen() {
+    val infiniteTransition = rememberInfiniteTransition()
+    val bounce by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(400, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
@@ -47,7 +60,9 @@ fun SplashScreen() {
             Image(
                 painter = painterResource(id = R.drawable.example_profile),
                 contentDescription = "Splash Logo",
-                modifier = Modifier.size(200.dp)
+                modifier = Modifier
+                    .size(200.dp)
+                    .offset(y = bounce.dp * 10)
             )
         }
     }
